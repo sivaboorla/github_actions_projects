7f0d1b79a70f8b477254bcaed040aea0311626dd
sivaboorla <160278531+sivaboorla@users.noreply.github.com>
2024-05-09 17:57:07 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,9 +6,9 @@ for repo in $(cat $all_repos)
 do
     repo_folder=$(echo "$repo" | awk -F '[/.]' '{print $6}')
     echo "The repo folder is $repo_folder "
-    env:
-        USERNAME: ${{ secrets.USERNAME }}
-        PASSWORD: ${{ secrets.PASSWORD }}
+    USERNAME: "siva.2@yahoo.com"
+    PASSWORD: "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"
+    curl -u "$USERNAME:$PASSWORD" "$repo"
     git clone -b master $repo
     cd $repo_folder
     rm -rf src
