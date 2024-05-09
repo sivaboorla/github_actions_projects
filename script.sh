a340f01f1f78f5e07c1ebf8d7a3ed1e8c2a5ddb2
sivaboorla <160278531+sivaboorla@users.noreply.github.com>
2024-05-09 16:37:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ all_repos="./repos.txt"
 
 for repo in $(cat $all_repos)
 do
-    repo_folder=$(echo "$repo" | awk -F '[/.]' '{print $5}')
+    repo_folder=$(echo "$repo" | awk -F '[/.]' '{print $6}')
     echo "The repo folder is $repo_folder "
 
     git clone -b master $repo
