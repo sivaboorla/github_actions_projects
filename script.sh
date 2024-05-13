7cce00a8260c38fbf3742809ff17a58951f27e3d
sivaboorla <160278531+sivaboorla@users.noreply.github.com>
2024-05-13 11:05:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,10 +9,10 @@ do
     USERNAME="sivaboorla" 
     CLONE_URL=$(https://$USERNAME:$TOKEN@repo)
     echo "$CLONE_URL"
-    git clone -b master $CLONE_URL
-    cd $repo_folder
-    rm -rf src
-    git add .
-    git commit -m "src folder deletion"
-    git push origin master
+    # git clone -b master $CLONE_URL
+    # cd $repo_folder
+    # rm -rf src
+    # git add .
+    # git commit -m "src folder deletion"
+    # git push origin master
 done
