980d308d3e747e895a735cfed287858f7dd58f0b
sivaboorla <160278531+sivaboorla@users.noreply.github.com>
2024-05-13 13:23:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,16 @@ echo "Hello world"
 #     repo_folder=$(echo "$repo" | awk -F '[/.]' '{print $6}')
 #     echo "The repo folder is $repo_folder "
 #     USERNAME="sivaboorla" 
+
+      git config --global user.email "siva.2@yahoo.com
+      git config --global user.name "sivaboorla"
+      
+      GH_TOKEN=$PASSWORD
+      git config credentials.helper 'store --file ~/.git-credentials' 
+      echo "https://$GH_TOKEN:x-0auth-basic">~/git-credentials
+
+      git clone -b main $repo
+      
 #     CLONE_URL=$(https://$USERNAME:$PAT_TOKEN@repo)
 #     echo "$CLONE_URL"
 #     # git clone -b master $CLONE_URL
