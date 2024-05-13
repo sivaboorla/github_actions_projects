b22488e326e70a371ce1273f062f07844fff12df
sivaboorla <160278531+sivaboorla@users.noreply.github.com>
2024-05-13 09:55:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,13 +6,17 @@ for repo in $(cat $all_repos)
 do
     repo_folder=$(echo "$repo" | awk -F '[/.]' '{print $6}')
     echo "The repo folder is $repo_folder "
-    USERNAME: "sivaboorla"
-    curl -u "$USERNAME:${PASSWORD}" "$repo"
-    git clone -b master $repo
-    cd $repo_folder
-    rm -rf src
-    git add .
-    git commit -m "src folder deletion"
+    USERNAME="sivaboorla"
+    echo "YOUR USERNAME IS $USERNAME"
+    echo "YOUR PASSWORD IS ${secreats.PASSWORD"
+    
+    #curl -u "$USERNAME:${PASSWORD}" "$repo"
+    # git clone https://$USERNAME:${PASSWORD}@repo
+    # git clone -b master $repo
+    # cd $repo_folder
+    # rm -rf src
+    # git add .
+    # git commit -m "src folder deletion"
 
-    git push origin master
+    # git push origin master
 done
